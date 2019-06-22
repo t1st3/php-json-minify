c032b27c02cade3c58119d2aa66e851e0225e65c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-06-23 00:38:27 +0200

    Ensure there is always something to return
    Especially when the regexp doesn't match anything.

--- tests/JSONMinTest.php
+++ tests/JSONMinTest.php
@@ -10,6 +10,11 @@ class JSONMinTest extends PHPUnit_Framework_TestCase
           '{"a": "b"}',
           '{"a":"b"}',
       ];
+
+      yield [
+          '[0, 1, 2, 3, 4, 5]',
+          '[0, 1, 2, 3, 4, 5]',
+      ];
   }
 
   /**
