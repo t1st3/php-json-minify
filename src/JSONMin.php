b24db7ae3ad34a7baa3979e068fad08262e51f10
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-02-12 08:29:24 +0100

    Modernize project a bit and increase test coverage

--- src/JSONMin.php
+++ src/JSONMin.php
@@ -1,90 +1,106 @@
 <?php
 /**
-* php-json-minify
-* @package JSONMin
-* @version 0.6.2
-* @link https://github.com/t1st3/php-json-minify
-* @author t1st3 <https://github.com/t1st3>
-* @license https://github.com/t1st3/php-json-minify/blob/master/LICENSE.md MIT
-* @copyright Copyright (c) 2014, t1st3
-* 
-* 
-* Based on JSON.minify (https://github.com/getify/JSON.minify) by Kyle Simspon (https://github.com/getify)
-* JSON.minify is released under MIT license.
-*
-*/
+ * php-json-minify
+ * @package JSONMin
+ * @version 0.6.2
+ * @link https://github.com/t1st3/php-json-minify
+ * @author t1st3 <https://github.com/t1st3>
+ * @license https://github.com/t1st3/php-json-minify/blob/master/LICENSE.md MIT
+ * @copyright Copyright (c) 2014, t1st3
+ *
+ *
+ * Based on JSON.minify (https://github.com/getify/JSON.minify) by Kyle Simspon (https://github.com/getify)
+ * JSON.minify is released under MIT license.
+ *
+ */
 
 namespace t1st3\JSONMin;
 
+use function implode;
+use function preg_match;
+use function preg_replace;
+use function strlen;
+use function substr;
+
 /**
-* The JSONMin class
-* @author t1st3 <https://github.com/t1st3>
-* @since 0.1.0
-*/
-class JSONMin {
+ * The JSONMin class
+ * @author t1st3 <https://github.com/t1st3>
+ * @since 0.1.0
+ */
+final class JSONMin
+{
+  /**
+   * The original JSON string
+   * @var string $original_json The original JSON string
+   * @since 0.1.0
+   */
+  private $original_json = '';
 
   /**
-  * The original JSON string
-  * @var string $original_json The original JSON string
-  * @since 0.1.0
-  */
-  protected $original_json = '';
+   * The minified JSON string
+   * @var string $minified_json The minified JSON string
+   * @since 0.1.0
+   */
+  private $minified_json = '';
 
   /**
-  * The minified JSON string
-  * @var string $minified_json The minified JSON string
-  * @since 0.1.0
-  */
-  protected $minified_json = '';
-
-  /**
-  * Constructor
-  * @name __construct
-  * @param string $json Some JSON to minify
-  * @since 0.1.0
-  * @return object the JSONMin object
-  */
-  public function __construct ( $json ) {
+   * Constructor
+   * @name __construct
+   * @param string $json Some JSON to minify
+   * @since 0.1.0
+   * @return object the JSONMin object
+   */
+  public function __construct(string $json)
+  {
     $this->original_json = $json;
     return $this;
   }
 
   /**
-  * Get the minified JSON
-  * @name getMin
-  * @since 0.1.0
-  * @return string Minified JSON string
-  */
-  public function getMin ( ) {
+   * Get the minified JSON
+   * @name getMin
+   * @since 0.1.0
+   * @return string Minified JSON string
+   */
+  public function getMin(): string
+  {
     $this->minified_json = $this::minify($this->original_json);
     return $this->minified_json;
   }
 
   /**
-  * Print the minified JSON
-  * @name printMin
-  * @since 0.1.0
-  * @return object the JSONMin object
-  */
-  public function printMin ( ) {
+   * Print the minified JSON
+   * @name printMin
+   * @since 0.1.0
+   * @return object the JSONMin object
+   */
+  public function printMin(): self
+  {
     echo $this->getMin();
     return $this;
   }
 
   /**
-  * Static minify function
-  * @name minify
-  * @param string $json Some JSON to minify
-  * @since 0.1.0
-  * @return string Minified JSON string
-  * @static
-  */
-  public static function minify ($json) {
+   * Static minify function
+   * @name minify
+   * @param string $json Some JSON to minify
+   * @since 0.1.0
+   * @return string Minified JSON string
+   * @static
+   */
+  public static function minify(string $json): string
+  {
     $tokenizer = "/\"|(\/\*)|(\*\/)|(\/\/)|\n|\r/";
     $in_string = false;
     $in_multiline_comment = false;
     $in_singleline_comment = false;
-    $tmp; $tmp2; $new_str = array(); $ns = 0; $from = 0; $lc; $rc; $lastIndex = 0;
+    $tmp = null;
+    $tmp2 = null;
+    $new_str = array();
+    $from = 0;
+    $lc = null;
+    $rc = null;
+    $lastIndex = 0;
     while (preg_match($tokenizer,$json,$tmp,PREG_OFFSET_CAPTURE,$lastIndex)) {
       $tmp = $tmp[0];
       $lastIndex = $tmp[1] + strlen($tmp[0]);
@@ -123,10 +139,9 @@ class JSONMin {
       }
     }
     if (!isset($rc)) {
-        $rc = $json;
+      $rc = $json;
     }
-    $new_str[] = $rc;
+    $new_str[] = preg_replace("/(\n|\r|\s)*/","",$rc);
     return implode("",$new_str);
   }
 }
-?>
